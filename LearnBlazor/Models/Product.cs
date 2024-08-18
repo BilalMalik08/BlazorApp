21a39670fcfbb42c55bc758488b27ad92d138ab1
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-08-19 02:41:54 +0500

    Updated 2:45 AM, 19 August, 2024

---
+++ LearnBlazor/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
+    }
+}

d5e2eee127988160d32b6483d40ee7debc9a6640
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-08-27 02:51:05 +0500

    Updated 2:55 AM, 27 August, 2024

---
+++ LearnBlazor/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async Task ToastrSuccess(this IJSRuntime jSRuntime, string message)
+        {
+            // Directly invoking the JavaScript function to show a success toast
+            await jSRuntime.InvokeVoidAsync("showToastr", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime jSRuntime, string message)
+        {
+            // Directly invoking the JavaScript function to show an error toast
+            await jSRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
---
+++ LearnBlazor/wwwroot/js/Common.js
@@ -0,0 +1,16 @@
+﻿window.showToastr = function (type, message) {
+    if (type === "success") {
+        toastr.success(message);
+    }
+    if (type === "error") {
+        toastr.error(message);
+    }
+}
+
+window.showSwal = function (type, message) {
+    Swal.fire({
+        title: type === "success" ? "Good job!" : "Error!",
+        text: message,
+        icon: type
+    });
+}
